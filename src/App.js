ad9013ae4cf0fe028937c7ae0a438fe0ccdbce20
James Twose <jamestwose@Mac-mini.home>
2022-01-03 23:16:31 +0100

    trying out some nivo plots

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-// import logo from './logo.svg';
 import './App.css';
+import food_data from './data/food.json';
+import travel_data from './data/country_travel.json';
+import weight_data from './data/size_weight.json';
+import MyResponsiveLine from "./line.js";
+import MyResponsiveBar from "./bar.js";
+import MyResponsiveScatterPlot from "./scatter.js";
+// import {csv} from "d3-fetch";
+// import {min} from "d3-array";
+
+
+// const iris_data = csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv").then(function(data) {
+//   console.log(data[0]);
+// });
+
+// const sepal_length_min = min(iris_data, d => d.sepal_length)
+
+// console.log(sepal_length_min);
+
+// var minLand = min(iris_data, function(d) { return d.sepal_length; });
+// console.log(minLand);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Examples of Nivo Plots</h1>
+        
+        <div style={{ width: 900, height: 290 }}>
+          <MyResponsiveScatterPlot data={weight_data} />
+        </div>
+
+        <div style={{ width: 900, height: 290 }}>
+          <MyResponsiveLine data={travel_data} />
+        </div>
+
+        <div style={{ width: 900, height: 290 }}>
+          <MyResponsiveBar data={food_data} />
+        </div>
+        
+
       </header>
     </div>
   );
